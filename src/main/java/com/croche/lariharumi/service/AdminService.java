feeb7e46f6889f4961a1a5c8dfa3a51aad9dad0f
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2024-12-16 16:30:14 -0300

    feat: create Admin Service

--- src/main/java/com/croche/lariharumi/service/AdminService.java
+++ src/main/java/com/croche/lariharumi/service/AdminService.java
@@ -2,36 +2,27 @@ package com.croche.lariharumi.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.croche.lariharumi.models.Category.Category;
 import com.croche.lariharumi.models.Product.Product;
 import com.croche.lariharumi.repository.ProductRepository;
 
-import java.util.List;
-
 @Service
-public class ProductService {
+public class AdminService {
 
     @Autowired
     private ProductRepository productRepository;
 
-    public Product createProduct(String name, String description, String price, Category category) {
-        Product product = new Product();
-        product.setName(name);
-        product.setDescription(description);
-        product.setPriceRange(price);
-        product.setCategory(category);  // Example, adjust as per your model
+    // Adicionar um novo produto
+    @Transactional
+    public Product addProduct(String name, String description, String price, Category category) {
+        Product product = new Product(null, name, description, price, price, category);
         return productRepository.save(product);
     }
 
-    public List<Product> getAllProducts() {
-        return productRepository.findAll();
-    }
-
-    public Product getProductById(Long id) {
-        return productRepository.findById(id).orElseThrow(() -> new RuntimeException("Product not found"));
-    }
-
+    // Atualizar um produto existente
+    @Transactional
     public Product updateProduct(Long id, String name, String description, String price, Category category) {
         Product product = productRepository.findById(id).orElseThrow(() -> new RuntimeException("Product not found"));
         product.setName(name);
@@ -41,6 +32,8 @@ public class ProductService {
         return productRepository.save(product);
     }
 
+    // Remover um produto
+    @Transactional
     public void deleteProduct(Long id) {
         Product product = productRepository.findById(id).orElseThrow(() -> new RuntimeException("Product not found"));
         productRepository.delete(product);
