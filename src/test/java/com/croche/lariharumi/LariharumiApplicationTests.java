44bb6492d24b65396ac509b979d21f1b6614faa5
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2024-12-16 14:45:47 -0300

    first commit

---
+++ src/test/java/com/croche/lariharumi/LariharumiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.croche.lariharumi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LariharumiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
