176f47349b95684861c8f6953d3d75f43269e3a6
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2024-12-16 15:21:38 -0300

    feat: fix connection errors

--- src/main/java/com/croche/lariharumi/models/User/User.java
+++ src/main/java/com/croche/lariharumi/models/User/User.java
@@ -6,11 +6,13 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "users")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
